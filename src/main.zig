01e9ed6ee2a8c4d7f7e293633fb6d791464d88a1
sineptic <sineptic0@gmail.com>
2025-12-01 22:02:43 +0300

    revise day 1 part 2

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ pub fn main() !void {
     defer allocator.free(input);
 
     std.debug.print("day 1 part 1: {}\n", .{try day_1_part_1.run(input)});
-    try day_1_part_2.run(allocator, input);
+    std.debug.print("day 1 part 2: {}\n", .{try day_1_part_2.run(input)});
 }
 
 pub fn load_input(allocator: std.mem.Allocator, day: u8) ![]const u8 {
@@ -71,64 +71,41 @@ const day_1_part_1 = struct {
     }
 };
 const day_1_part_2 = struct {
-    const Rotation = struct {
-        direction: Direction,
-        steps: usize,
-
-        const Direction = enum { left, right };
-    };
-    pub fn run(allocator: std.mem.Allocator, input: []const u8) !void {
-        var lines_iter = std.mem.splitScalar(u8, input, '\n');
-        var lines = std.ArrayList([]const u8).empty;
-        defer lines.deinit(allocator);
-        while (lines_iter.next()) |line| {
-            try lines.append(allocator, line);
-        }
-
-        var rotations = std.ArrayList(Rotation).empty;
-        defer rotations.deinit(allocator);
-        for (lines.items) |line| {
-            if (line.len == 0) {
-                continue;
-            }
-            const direction_letter = line[0];
-            const direction = a: switch (direction_letter) {
-                'L' => {
-                    break :a Rotation.Direction.left;
-                },
-                'R' => {
-                    break :a Rotation.Direction.right;
-                },
-                else => {
-                    std.debug.panic("unexpected first character in line: {c}", .{direction_letter});
-                },
-            };
-            const remainder = line[1..];
-            const steps = try std.fmt.parseInt(usize, remainder, 10);
-            const rotation = Rotation{ .direction = direction, .steps = steps };
-            try rotations.append(allocator, rotation);
-        }
+    pub fn run(input: []const u8) !i64 {
+        var lines = std.mem.splitScalar(u8, input, '\n');
 
         var position: i16 = 50;
         var count: usize = 0;
-        for (rotations.items) |rotation| {
-            const steps: usize = @intCast(rotation.steps);
-            for (0..steps) |_| {
-                switch (rotation.direction) {
-                    .left => {
+        while (lines.next()) |line| {
+            if (line.len == 0) {
+                continue;
+            }
+            const steps = try std.fmt.parseInt(usize, line[1..], 10);
+            switch (line[0]) {
+                'L' => {
+                    for (0..steps) |_| {
                         position -= 1;
-                    },
-                    .right => {
+                        position = @mod(position, 100);
+                        if (position == 0) {
+                            count += 1;
+                        }
+                    }
+                },
+                'R' => {
+                    for (0..steps) |_| {
                         position += 1;
-                    },
-                }
-                position = @mod(position, 100);
-                if (position == 0) {
-                    count += 1;
-                }
+                        position = @mod(position, 100);
+                        if (position == 0) {
+                            count += 1;
+                        }
+                    }
+                },
+                else => {
+                    @panic("unexpected first character in line");
+                },
             }
         }
-        std.debug.print("day 1 part 2: {}\n", .{count});
+        return @intCast(count);
     }
 };
 
@@ -140,17 +117,6 @@ test "day 1" {
     const answer_1 = try day_1_part_1.run(input);
     try std.testing.expectEqual(1145, answer_1);
 
-    const answer_2 = try day_1_part_2.run(gpa, input);
+    const answer_2 = try day_1_part_2.run(input);
     try std.testing.expectEqual(6561, answer_2);
 }
-
-// test "fuzz example" {
-//     const Context = struct {
-//         fn testOne(context: @This(), input: []const u8) anyerror!void {
-//             _ = context;
-//             // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-//             try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-//         }
-//     };
-//     try std.testing.fuzz(Context{}, Context.testOne, .{});
-// }
