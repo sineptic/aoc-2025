3a7fca6b91dd0af9b0a7f7ebfcd07159943dc308
sineptic <sineptic0@gmail.com>
2025-12-01 21:30:15 +0300

    add test

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,7 @@ pub fn main() !void {
     const input = try load_input(allocator, 1);
     defer allocator.free(input);
 
-    try day_1_part_1.run(allocator, input);
+    std.debug.print("day 1 part 1: {}\n", .{try day_1_part_1.run(input)});
     try day_1_part_2.run(allocator, input);
 }
 
@@ -28,17 +28,12 @@ const day_1_part_1 = struct {
 
         const Direction = enum { left, right };
     };
-    pub fn run(allocator: std.mem.Allocator, input: []const u8) !void {
-        var lines_iter = std.mem.splitScalar(u8, input, '\n');
-        var lines = std.ArrayList([]const u8).empty;
-        defer lines.deinit(allocator);
-        while (lines_iter.next()) |line| {
-            try lines.append(allocator, line);
-        }
+    pub fn run(input: []const u8) !i64 {
+        var lines = std.mem.splitScalar(u8, input, '\n');
 
-        var rotations = std.ArrayList(Rotation).empty;
-        defer rotations.deinit(allocator);
-        for (lines.items) |line| {
+        var position: i16 = 50;
+        var count: usize = 0;
+        while (lines.next()) |line| {
             if (line.len == 0) {
                 continue;
             }
@@ -57,13 +52,7 @@ const day_1_part_1 = struct {
             const remainder = line[1..];
             const steps = try std.fmt.parseInt(i16, remainder, 10);
             const rotation = Rotation{ .direction = direction, .steps = steps };
-            try rotations.append(allocator, rotation);
-        }
 
-        var position: i16 = 50;
-        var count: usize = 0;
-        for (rotations.items) |rotation| {
-            // std.debug.print("{}\n", .{rotation});
             switch (rotation.direction) {
                 .left => {
                     position -= rotation.steps;
@@ -77,13 +66,14 @@ const day_1_part_1 = struct {
                 count += 1;
             }
         }
-        std.debug.print("day 1 part 1: {}\n", .{count});
+
+        return @intCast(count);
     }
 };
 const day_1_part_2 = struct {
     const Rotation = struct {
         direction: Direction,
-        steps: i16,
+        steps: usize,
 
         const Direction = enum { left, right };
     };
@@ -114,7 +104,7 @@ const day_1_part_2 = struct {
                 },
             };
             const remainder = line[1..];
-            const steps = try std.fmt.parseInt(i16, remainder, 10);
+            const steps = try std.fmt.parseInt(usize, remainder, 10);
             const rotation = Rotation{ .direction = direction, .steps = steps };
             try rotations.append(allocator, rotation);
         }
@@ -142,13 +132,17 @@ const day_1_part_2 = struct {
     }
 };
 
-// test "simple test" {
-//     const gpa = std.testing.allocator;
-//     var list: std.ArrayList(i32) = .empty;
-//     defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-//     try list.append(gpa, 42);
-//     try std.testing.expectEqual(@as(i32, 42), list.pop());
-// }
+test "day 1" {
+    const gpa = std.testing.allocator;
+    const input = try load_input(gpa, 1);
+    defer gpa.free(input);
+
+    const answer_1 = try day_1_part_1.run(input);
+    try std.testing.expectEqual(1145, answer_1);
+
+    const answer_2 = try day_1_part_2.run(gpa, input);
+    try std.testing.expectEqual(6561, answer_2);
+}
 
 // test "fuzz example" {
 //     const Context = struct {
