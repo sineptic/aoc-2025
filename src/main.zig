5545af4b6f8c545a2d7d54df43f9945d5762943d
sineptic <sineptic0@gmail.com>
2025-12-03 20:34:50 +0300

    day 2

--- src/main.zig
+++ src/main.zig
@@ -6,16 +6,27 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const input = try load_input(allocator, 1);
-    defer allocator.free(input);
+    const input1 = try load_input(allocator, 1);
+    defer allocator.free(input1);
+    const input2 = try load_input(allocator, 2);
+    defer allocator.free(input2);
 
-    std.debug.print("day 1 part 1: {}\n", .{try day_1_part_1.run(input)});
-    std.debug.print("day 1 part 2: {}\n", .{try day_1_part_2.run(input)});
+    std.debug.print("day 1 part 1: {}\n", .{try day_1_part_1.run(input1)});
+    std.debug.print("day 1 part 2: {}\n", .{try day_1_part_2.run(input1)});
+    std.debug.print("day 2 part 1: {}\n", .{try day_2.run1(input2)});
+    std.debug.print("day 2 part 2: {}\n", .{try day_2.run2(input2)});
 }
 
 pub fn load_input(allocator: std.mem.Allocator, day: u8) ![]const u8 {
     assert(day > 0);
     const file_path = try std.fmt.allocPrint(allocator, "inputs/day-{}.txt", .{day});
+    defer allocator.free(file_path);
+    const content = try std.fs.cwd().readFileAlloc(allocator, file_path, 100_000);
+    return content;
+}
+pub fn load_example_input(allocator: std.mem.Allocator, day: u8) ![]const u8 {
+    assert(day > 0);
+    const file_path = try std.fmt.allocPrint(allocator, "inputs/day-{}-example.txt", .{day});
     defer allocator.free(file_path);
     const content = try std.fs.cwd().readFileAlloc(allocator, file_path, 100_000);
     return content;
@@ -85,8 +96,77 @@ const day_1_part_2 = struct {
         return @intCast(count);
     }
 };
+const day_2 = struct {
+    fn run1(input: []const u8) !i64 {
+        return run(input, validate_day_1);
+    }
+    fn run2(input: []const u8) !i64 {
+        return run(input, validate_day_2);
+    }
 
-test "day 1" {
+    fn run(input: []const u8, validate_length: fn (usize, usize) bool) !i64 {
+        const mb_line_end = std.mem.indexOfScalar(u8, input, '\n');
+        const first_line = a: {
+            if (mb_line_end) |line_end| {
+                break :a input[0..line_end];
+            } else {
+                break :a input;
+            }
+        };
+        var ranges = std.mem.splitScalar(u8, first_line, ',');
+
+        var result: i64 = 0;
+        var number_buffer: [1000]u8 = undefined;
+        var number_buffer2 = std.heap.FixedBufferAllocator.init(&number_buffer);
+        const number_allocator = number_buffer2.allocator();
+        while (ranges.next()) |range| {
+            const dash_pos = std.mem.indexOfScalar(u8, range, '-') orelse unreachable;
+            const left_raw = range[0..dash_pos];
+            const right_raw = range[dash_pos + 1 ..];
+            const left = try std.fmt.parseInt(u64, left_raw, 10);
+            const right = try std.fmt.parseInt(u64, right_raw, 10);
+
+            for (left..right + 1) |number| {
+                const printed_number = try std.fmt.allocPrint(number_allocator, "{}", .{number});
+                defer number_allocator.free(printed_number);
+                if (is_repeated(printed_number, validate_length)) {
+                    result += int_cast(i64, number);
+                }
+            }
+        }
+        return result;
+    }
+    fn validate_day_1(total_length: usize, sequence_length: usize) bool {
+        return total_length / sequence_length == 2;
+    }
+    fn validate_day_2(total_length: usize, sequence_length: usize) bool {
+        return total_length / sequence_length >= 2;
+    }
+    fn is_repeated(number: []const u8, validate_length: fn (usize, usize) bool) bool {
+        outer: for (1..number.len) |length| {
+            if (@mod(number.len, length) != 0) {
+                continue;
+            }
+            if (@mod(number.len, number.len / length) != 0) {
+                continue;
+            }
+            if (!validate_length(number.len, length)) {
+                continue;
+            }
+            for (0..number.len) |i| {
+                const left = number[i];
+                const right = number[@mod(i, length)];
+                if (left != right) {
+                    continue :outer;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+};
+
+test "day_1" {
     const gpa = std.testing.allocator;
     const input = try load_input(gpa, 1);
     defer gpa.free(input);
@@ -96,4 +176,16 @@ test "day 1" {
 
     const answer_2 = try day_1_part_2.run(input);
     try std.testing.expectEqual(6561, answer_2);
+}
+
+test "day_2" {
+    const gpa = std.testing.allocator;
+    const input = try load_input(gpa, 2);
+    defer gpa.free(input);
+
+    const answer_1 = try day_2.run1(input);
+    try std.testing.expectEqual(40398804950, answer_1);
+
+    const answer_2 = try day_2.run2(input);
+    try std.testing.expectEqual(65794984339, answer_2);
 }
