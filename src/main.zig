f012cdaf00cc48eb622925dd49327ea27e4a58ce
sineptic <sineptic0@gmail.com>
2025-12-07 17:59:12 +0300

    day 2: precalculate possible chunks for number sizes

--- src/main.zig
+++ src/main.zig
@@ -11,10 +11,12 @@ pub fn main() !void {
     const input2 = try load_input(allocator, 2);
     defer allocator.free(input2);
 
+    // for (0..100) |_| {
     std.debug.print("day 1 part 1: {}\n", .{try day_1_part_1.run(input1)});
     std.debug.print("day 1 part 2: {}\n", .{try day_1_part_2.run(input1)});
     std.debug.print("day 2 part 1: {}\n", .{try day_2.run1(input2)});
     std.debug.print("day 2 part 2: {}\n", .{try day_2.run2(input2)});
+    // }
 }
 
 pub fn load_input(allocator: std.mem.Allocator, day: u8) ![]const u8 {
@@ -98,13 +100,13 @@ const day_1_part_2 = struct {
 };
 const day_2 = struct {
     fn run1(input: []const u8) !u64 {
-        return run(input, is_repeated1);
+        return run(input, solve_range1);
     }
     fn run2(input: []const u8) !u64 {
-        return run(input, is_repeated2);
+        return run(input, solve_range2);
     }
 
-    fn run(input: []const u8, is_repeated: fn ([]const u8) bool) !u64 {
+    fn run(input: []const u8, solve_range: fn ([]const u8, []const u8) u64) !u64 {
         const mb_line_end = std.mem.indexOfScalar(u8, input, '\n');
         const first_line = a: {
             if (mb_line_end) |line_end| {
@@ -120,11 +122,13 @@ const day_2 = struct {
             const dash_pos = std.mem.indexOfScalar(u8, range, '-') orelse unreachable;
             const left_raw = range[0..dash_pos];
             const right_raw = range[dash_pos + 1 ..];
-            result += solve_range(left_raw, right_raw, is_repeated);
+            result += solve_range(left_raw, right_raw);
         }
         return result;
     }
-    fn solve_range(left_raw: []const u8, right_raw: []const u8, is_repeated: fn ([]const u8) bool) u64 {
+    fn solve_range1(left_raw: []const u8, right_raw: []const u8) u64 {
+        assert(right_raw.len <= 10);
+
         var number_buffer: [1000]u8 = undefined;
         @memcpy(number_buffer[0..left_raw.len], left_raw);
 
@@ -135,8 +139,67 @@ const day_2 = struct {
         var current_raw = number_buffer[0..left_raw.len];
         var current = left;
         while (current <= right) {
-            if (is_repeated(current_raw)) {
+            if (@mod(current_raw.len, 2) != 0) {
+                @memset(number_buffer[0..], '0');
+                number_buffer[0] = '1';
+                current_raw = number_buffer[0 .. current_raw.len + 1];
+                current = std.math.pow(u64, 10, current_raw.len - 1);
+                continue;
+            }
+            if (std.mem.eql(
+                u8,
+                current_raw[0 .. current_raw.len / 2],
+                current_raw[current_raw.len / 2 .. current_raw.len],
+            )) {
                 result += current;
+            }
+
+            current += 1;
+            const fit = increment(current_raw);
+            if (!fit) {
+                @memset(number_buffer[0..], '0');
+                number_buffer[0] = '1';
+                current_raw = number_buffer[0 .. current_raw.len + 1];
+            }
+        }
+        return result;
+    }
+    const possible_chunk_sizes = [11][]const usize{
+        &[_]usize{},
+        &[_]usize{},
+        &[_]usize{1},
+        &[_]usize{1},
+        &[_]usize{ 1, 2 },
+        &[_]usize{1},
+        &[_]usize{ 1, 2, 3 },
+        &[_]usize{1},
+        &[_]usize{ 1, 2, 4 },
+        &[_]usize{ 1, 3 },
+        &[_]usize{ 1, 2, 5 },
+    };
+    fn solve_range2(left_raw: []const u8, right_raw: []const u8) u64 {
+        assert(right_raw.len <= 10);
+
+        var number_buffer: [1000]u8 = undefined;
+        @memcpy(number_buffer[0..left_raw.len], left_raw);
+
+        const left = std.fmt.parseInt(u64, left_raw, 10) catch unreachable;
+        const right = std.fmt.parseInt(u64, right_raw, 10) catch unreachable;
+
+        var result: u64 = 0;
+        var current_raw = number_buffer[0..left_raw.len];
+        var current = left;
+        while (current <= right) {
+            const possible_sizes = possible_chunk_sizes[current_raw.len];
+            inner: for (possible_sizes) |size| {
+                if (std.mem.eql(
+                    u8,
+                    current_raw[0 .. current_raw.len - size],
+                    current_raw[size..current_raw.len],
+                )) {
+                    result += current;
+                    break :inner;
+                }
             }
 
             current += 1;
@@ -165,29 +228,6 @@ const day_2 = struct {
             else => unreachable,
         }
     }
-    fn is_repeated1(number: []const u8) bool {
-        const length = number.len / 2;
-        if (number.len != length * 2) {
-            return false;
-        }
-        return std.mem.eql(
-            u8,
-            number[0 .. number.len - length],
-            number[length..number.len],
-        );
-    }
-    fn is_repeated2(number: []const u8) bool {
-        for (1..number.len / 2 + 1) |length| {
-            if (std.mem.eql(
-                u8,
-                number[0 .. number.len - length],
-                number[length..number.len],
-            )) {
-                return @mod(number.len, length) == 0;
-            }
-        }
-        return false;
-    }
 };
 
 test "day_1" {
@@ -206,6 +246,9 @@ test "day_2" {
     const gpa = std.testing.allocator;
     const input = try load_input(gpa, 2);
     defer gpa.free(input);
+
+    const a = try day_2.run2("121212-121213");
+    try std.testing.expectEqual(121212, a);
 
     const answer_1 = try day_2.run1(input);
     try std.testing.expectEqual(40398804950, answer_1);
